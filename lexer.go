4f7541344653303618756c6205bcf34f398bd403
LunarVQ <126401749+LunarVQ@users.noreply.github.com>
2024-09-25 22:54:42 -0400

    added characters and character literals

--- lexer.go
+++ lexer.go
@@ -9,20 +9,21 @@ import (
 type TokenType string
 
 const (
-	TokenKeyword    TokenType = "KEYWORD"
-	TokenIdentifier TokenType = "IDENTIFIER"
-	TokenNumber     TokenType = "NUMBER"
-	TokenSymbol     TokenType = "SYMBOL"
-	TokenEOF        TokenType = "EOF"
+	TokenKeyword     TokenType = "KEYWORD"
+	TokenIdentifier  TokenType = "IDENTIFIER"
+	TokenNumber      TokenType = "NUMBER"
+	TokenSymbol      TokenType = "SYMBOL"
+	TokenCharLiteral TokenType = "CHAR_LITERAL" // New token type for char literals
+	TokenEOF         TokenType = "EOF"
 )
 
-// Token struct with type and value
+// Token structure with type and value
 type Token struct {
 	Type  TokenType
 	Value string
 }
 
-// Define KeyWords type and a set of keywords
+// Define KeyWord type and a set of keywords
 type KeyWord string
 
 const (
@@ -40,6 +41,7 @@ const (
 	KeyWordDefault KeyWord = "default"
 	KeyWordSwitch  KeyWord = "switch"
 	KeyWordCase    KeyWord = "case"
+	KeyWordChar    KeyWord = "char" // char keyword
 )
 
 // A map of keywords for easy lookup
@@ -58,6 +60,7 @@ var keywords = map[string]KeyWord{
 	"default": KeyWordDefault,
 	"switch":  KeyWordSwitch,
 	"case":    KeyWordCase,
+	"char":    KeyWordChar, // Added KeyWordChar to the map
 }
 
 // Tokenizer structure with input and current position
@@ -110,6 +113,11 @@ func (t *Tokenizer) GetNextToken() Token {
 		return t.readNumber()
 	}
 
+	// Detect character literals: sequences like 'a'
+	if t.currentChar == '\'' {
+		return t.readCharLiteral()
+	}
+
 	if t.currentChar == 0 {
 		return Token{Type: TokenEOF, Value: ""}
 	}
@@ -151,6 +159,19 @@ func (t *Tokenizer) readNumber() Token {
 		t.advance()
 	}
 	return Token{Type: TokenNumber, Value: t.input[start : t.position-1]}
+}
+
+// Read char literal token
+func (t *Tokenizer) readCharLiteral() Token {
+	t.advance() // Skip the opening single quote
+	charValue := t.currentChar
+	t.advance() // Move past the character
+	if t.currentChar == '\'' {
+		t.advance() // Skip the closing single quote
+		return Token{Type: TokenCharLiteral, Value: string(charValue)}
+	}
+	// Handle error case (e.g., if closing quote is missing)
+	return Token{Type: TokenSymbol, Value: string(charValue)} // Could also return an error token
 }
 
 // Tokenize the entire input
